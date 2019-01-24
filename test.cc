4fea2ec4ec9248ca7f192c17c9bdba39325fde02
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2019-01-24 10:35:18 +0800

    modified:   a.out 	modified:   test.cc

--- test.cc
+++ test.cc
@@ -1,13 +1,21 @@
 #include"ConcreteSystem.h"
 #include<iostream>
 #include<cstring>
+double raise(long d)
+{
+	double r = 1;
+	for (long i=0; i<d-1; ++i)
+		r *= 100;
+	return r;
+}
 int main(int argc, char **argv)
 {
-	double bigM = 2;
+	long digits = 0;
 
 	if (argc >= 2)
-		bigM = std::atof(argv[1]);
+		digits  = std::atoi(argv[1]);
 
+	double bigM = raise(digits);
 	Environment env(bigM);
 	while (env.nextState());
 	std::cout<<"num of collisions "<<env.EventsList.size()<<std::endl;
